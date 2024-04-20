6a6eb53296c408c6e1edf82b3d23c8982fcf4408
cropthecoder <myracropper32@gmail.com>
2024-04-20 12:28:49 -0400

    working model

--- model.py
+++ model.py
@@ -1,12 +1,12 @@
 import numpy as np 
 import pandas as pd
 import pickle
-import seaborn as sns
-import matplotlib.pyplot as plt  
+#import seaborn as sns
+#import matplotlib.pyplot as plt  
 from imblearn.over_sampling import RandomOverSampler
 from sklearn.impute import SimpleImputer
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import ConfusionMatrixDisplay , classification_report ,f1_score
+from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import make_pipeline
@@ -24,29 +24,32 @@ df.describe()
 
 #corr = df.drop(columns=['Class']).corr()
 #sns.heatmap(corr);
-plt.style.use("seaborn")
+#plt.style.use("seaborn")
 
-plt.rcParams['figure.figsize']= (22,11)
+#plt.rcParams['figure.figsize']= (22,11)
 
-plt.title("Correlation Heatmap",fontsize=18, weight= 'bold')
+#plt.title("Correlation Heatmap",fontsize=18, weight= 'bold')
 
-sns.heatmap(df.corr(), cmap="BuPu", annot=True)
+#sns.heatmap(df.corr(), cmap="BuPu", annot=True)
 
-plt.show()
+#plt.show()
 
-df['Class'].value_counts(normalize= True).plot(kind= 'bar')
-plt.xlabel("Class Distribution")
-plt.ylabel("Frequancy")
-plt.title("Class balance");
+#df['Class'].value_counts(normalize= True).plot(kind= 'bar')
+#plt.xlabel("Class Distribution")
+#plt.ylabel("Frequancy")
+#plt.title("Class balance");
 
-x= df.drop(['id', 'Class'], axis= 1)
-y= df['Class']
+if 'id' in df.columns:
+    df = df.drop(['id'], axis=1)
+
+x = df.drop(['Class'], axis=1)
+y = df['Class']
 
 stn_scaler = StandardScaler()
 
 x_scaled = stn_scaler.fit_transform(x)
 
-X = pd.DataFrame(x_scaled,columns=x.columns)
+X = pd.DataFrame(x_scaled, columns=x.columns)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 print("X shape:", X.shape)
@@ -58,20 +61,22 @@ print("X_test shape:", X_test.shape)
 print("y_test shape:", y_test.shape)
 
 acc_baseline = y_train.value_counts(normalize=True).max()
+
 print("Baseline Accuracy:", round(acc_baseline, 4))
 
 clf = LogisticRegression()
 
-clf.fit(X_train , y_train)
+clf.fit(X_train, y_train)
 
 clf.predict(X_train)
 
-acc_train = clf.score(X_train , y_train)
-acc_test = clf.score(X_test , y_test)
+acc_train = clf.score(X_train, y_train)
+acc_test = clf.score(X_test, y_test)
 
-print(f"Training accuracy: {round(acc_train , 4)}")
-print(f"test accuracy: {round(acc_test , 4)}")
+print(f"Training accuracy: {round(acc_train, 4)}")
+print(f"Test accuracy: {round(acc_test, 4)}")
 
+"""
 ConfusionMatrixDisplay.from_estimator(
 
     clf,
@@ -90,8 +95,11 @@ print(classification_report(
 features = X_test.columns
 importances = clf.coef_[0]
 
-feat_imp = pd.Series(importances , index=features).sort_values()
-feat_imp.tail().plot(kind= 'barh')
+feat_imp = pd.Series(importances, index=features).sort_values()
+feat_imp.tail().plot(kind='barh')
 plt.xlabel("Scale Importance")
 plt.ylabel("Feature")
 plt.title("Feature Importance");
+"""
+
+
